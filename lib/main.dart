a60c37dd115b5b6938744d54d9e58266faa1fb20
ArafatRohan93 <rohanarafat86@gmail.com>
2020-06-14 02:52:49 +0600

    Challange Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.fingerprint),
+          centerTitle: true ,
+          backgroundColor: Colors.indigo.shade900,
+          title: Text('Ask Anything'),
+        ),
+        body: AnsPage(),
+      ),
+    ),
+  );
+}
+
+class AnsPage extends StatefulWidget {
+  @override
+  _AnsPageState createState() => _AnsPageState();
+}
+
+class _AnsPageState extends State<AnsPage> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.indigo.shade600,
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    ballNumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$ballNumber.png'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
